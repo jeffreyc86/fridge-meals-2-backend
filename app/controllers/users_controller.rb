a8f2a82c1caa398c75a26779cbd7be815ca980de
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-07-22 16:06:09 -0400

    show path complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+        session[:user_id] = @current_user.id
+        render json: @current_user
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :favorites
   resources :ingredients
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/show", to: "users#show"
+
+  
 end
